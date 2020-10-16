aeb99f67771320a1c6740863f2d59ec971d6b819
Sarah Raines <saraines@seas.upenn.edu>
2020-10-15 20:43:07 -0400

    form validation, add confirm password, and remove unnecessary files

--- client/src/forms/AuthForm.jsx
+++ client/src/forms/AuthForm.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
+// import axios from 'axios';
 import "antd/dist/antd.css";
 import "./AuthForm.css"
 
 const AuthForm = ({isRegister}) => {
   const onFinish = values => {
-    console.log('Received values of form: ', values);
+    // axios.post(`https://jsonplaceholder.typicode.com/users`)
+    // .then(res => {
+    //   const persons = res.data;
+    //   this.setState({ persons });
+    // })
   };
 
   const submitButtonText = isRegister ? "Create account" : "Log in";
   const switchAuthPages = isRegister ? (
         <React.Fragment>
-            New user? <Link to="/register">Create account</Link>
+            Already have an account? <Link to="/login">Log in</Link>
         </React.Fragment>
     ) : (
         <React.Fragment>
-            Already have an account? <Link to="/login">Log in</Link>
+            New user? <Link to="/register">Create account</Link>
         </React.Fragment> 
     );
 
@@ -31,20 +36,52 @@ const AuthForm = ({isRegister}) => {
         >
         <Form.Item
             name="email"
-            rules={[{ required: true, message: 'Email is a required field.' }]}
+            hasFeedback
+            rules={[
+                { required: true, message: 'Email is a required field.' },
+                { type: 'email', message: 'Not a valid email.' }
+            ]}
         >
             <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
         </Form.Item>
         <Form.Item
             name="password"
-            rules={[{ required: true, message: 'Password is a required field.' }]}
+            hasFeedback={isRegister}
+            rules={[
+                { required: true, message: 'Password is a required field.' },
+                ...isRegister ? [{ min: 12, message: "Password must be at least 12 characters long."}] : [],
+            ]}
         >
             <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
             />
         </Form.Item>
+        {isRegister &&
+            <Form.Item
+                name="confirm"
+                dependencies={['password']}
+                hasFeedback
+                rules={[
+                    { required: true, message: 'Please confirm your password.' },
+                    ({ getFieldValue }) => ({
+                        validator(rule, value) {
+                            if (!value || getFieldValue('password') === value) {
+                            return Promise.resolve();
+                            }
+                            return Promise.reject('Passwords do not match.');
+                        },
+                    }),
+                ]}
+            >
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Confirm password"
+                />
+            </Form.Item>
+        }
         {!isRegister &&
             <Form.Item>
                 <Form.Item name="remember" valuePropName="checked" noStyle>
