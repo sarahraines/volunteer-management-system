490ae0341bf026443c206853e89764505eea9c0e
Jasmine Lee <leej1721@wharton.upenn.edu>
2020-11-17 06:39:00 -0500

    added feed to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <Route path="/events" component={Event} />
+          <Route path="/feed" component={Feed} />
       </Switch> 
   </main>
   );
