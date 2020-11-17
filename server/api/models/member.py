e9b7087525e0a8270bb0f11c5b01ba2df9b10f1c
Jasmine Lee <leej1721@wharton.upenn.edu>
2020-11-17 16:10:33 -0500

    redesigned member model to fix previous mistakes

--- server/api/models/member.py
+++ server/api/models/member.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from api.models import Organization
 User = settings.AUTH_USER_MODEL
 
 MEMBER_TYPES = (
@@ -9,5 +10,5 @@ MEMBER_TYPES = (
 
 class Member(models.Model):
     username = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
-    organizations = models.ManyToManyField('Organization')
+    organizations = models.ForeignKey(Organization, null=True, blank=True, on_delete=models.CASCADE)
     member_type = models.IntegerField(choices=MEMBER_TYPES)
