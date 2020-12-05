4cbd369f2111df9a29f53888b78c8232d2caa6f4
Jasmine Lee <leej1721@wharton.upenn.edu>
2020-12-05 11:47:34 -0500

    troubleshooting

--- server/api/models/clearance.py
+++ server/api/models/clearance.py
@@ -1,8 +1,11 @@
 from django.db import models
 from api.models import Organization
+
+
 class OrgFile(models.Model):
     organizations = models.ForeignKey(Organization, null=True, blank=True, on_delete=models.CASCADE)
     empty_form = models.FileField()
+
 class UserFile(models.Model):
     org_file = models.ForeignKey(OrgFile, null=True, blank=True, on_delete=models.CASCADE)
     filled_form = models.FileField()
