af882593236d3a2d9b5458c82d39eb005613a2f0
Jasmine Lee <leej1721@wharton.upenn.edu>
2020-12-19 16:50:56 -0500

    backend not working

--- client/src/pages/Clearances.jsx
+++ client/src/pages/Clearances.jsx
@@ -26,75 +26,78 @@ function Clearances({isAdmin, orgId}) {
     const [fileList, setFileList] = useState([]);
     //front end demo https://codesandbox.io/s/s98mf
     const props = {
-    action: '',
-    listType: 'picture',
-    previewFile(file) {
-      console.log('Your upload file:', file);
-      // Your process logic. Here we just mock to the same file
-        return axiosAPI.post('clearances/upload-user-file', {
-            file,
-            orgId 
-        },  {
-            headers: {
-            'Content-Type': 'multipart/form-data'
-            }
-        })
-        // .then(res => res.json())
-        .then(({ thumbnail }) => thumbnail);
-    },
-  };
+        action: '',
+        listType: 'picture',
+        previewFile(file) {
+            console.log('Your upload file:', file);
+            // Your process logic. Here we just mock to the same file
+                return axiosAPI.post('clearances/upload-user-file', {
+                    file,
+                    orgId 
+                },  {
+                    headers: {
+                    'Content-Type': 'multipart/form-data'
+                    }
+                })
+                // .then(res => res.json())
+                .then(({ thumbnail }) => thumbnail);
+        }
+    };
 //   useEffect(() => {
 //     getOrgFiles()
 // }, [orgId]);
 
-  const orgProps = {
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    onChange( info) {
-        if (info.file.status !== 'uploading') {
-          console.log(info.file, info.fileList);
-        }
-        if (info.file.status === 'done') {
-          message.success(`${info.file.name} file uploaded successfully`);
-        } else if (info.file.status === 'error') {
-          message.error(`${info.file.name} file upload failed.`);
-        }
-        let fl = [...info.fileList];
-        fl = fl.slice(-2);
-        fl = fl.map(file => {
-            if (file.response) {
-              // Component will show file.url as link
-              console.log(file)
-              file.url = "http://localhost:8080/"+ file.name;
-            }
-            return file;
-          });
-      
-          setFileList(fl);
-      },
-    previewFile: async function(file) {
-      const formData = new FormData();
-      console.log("hitting preview file")
-      formData.append('empty_form', file, file.name);
-      formData.append('orgId', orgId);
-      // Your process logic. Here we just mock to the same file
-        try {
-            return await axiosAPI.post('clearances/upload-org-file/', formData,{
-                headers: {
-                // 'Content-Type': 'multipart/form-data'
-                'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
-    
-                }
-            }
-            )
-            // .then(res => res.json())
-            .then(({ thumbnail }) => thumbnail);
+async function test(file) {
+    const formData = new FormData();
+    console.log("hitting preview file")
+    formData.append('empty_form', file, file.name);
+    formData.append('orgId', orgId);
+    // Your process logic. Here we just mock to the same file
+      try {
+          return await axiosAPI.post('clearances/upload-org-file/', formData,{
+              headers: {
+              // 'Content-Type': 'multipart/form-data'
+              'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+  
+              }
+          }
+          )
+          // .then(res => res.json())
+          .then(({ thumbnail }) => thumbnail);
 
-        } catch {
-            console.log("upload failed")
-        }
-        
-    },
-  };
+      } catch {
+          console.log("upload failed")
+      }
+  }
+
+    const orgProps = {
+        action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+        onChange( info) {
+            if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+            }
+            if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+            } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+            }
+            let fl = [...info.fileList];
+            fl = fl.slice(-2);
+            fl = fl.map(file => {
+                if (file.response) {
+                // Component will show file.url as link
+                console.log(file)
+                file.url = "http://localhost:8080/"+ file.name;
+                }
+                return file;
+            });
+            
+            setFileList(fl);
+        },
+        previewFile: test(file)
+    };
+
+    
 
     return (
         <div>
