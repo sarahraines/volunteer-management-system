c558901b8475c45b82dec402bb01ec879e3d9f85
Jasmine Lee <leej1721@wharton.upenn.edu>
2020-12-20 20:00:14 -0500

    user file and org files now completely linked and working

--- client/src/pages/UserFilesTable.jsx
+++ client/src/pages/UserFilesTable.jsx
@@ -11,24 +11,26 @@ function UserFilesTable({orgId, fileList, messageHandler}) {
         onChange(info) {
           messageHandler(info)
         },
-        previewFile(file) {
-          console.log('Your upload file:', file);
-            const formData = new FormData();
-            // console.log('file list', file.uid)
-            console.log("org id" +orgId)
-            console.log("org id above")
-            formData.append('org_file_id', orgId);
-            formData.append('user_id', localStorage.getItem("user_id"))
-            formData.append('filled_form', file, file.name);
-    
-            return axiosAPI.post('clearances/upload-user-file', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
-                }
-            })
-            .then(({ thumbnail }) => thumbnail);
-        },
+        
     };
+
+    function previewFile(orgFileId, file) {
+        console.log('Your upload file:', file);
+        const formData = new FormData();
+        // console.log('file list', fileList[0].uid)
+        console.log("org file id" + orgFileId)
+        console.log("org file id above")
+        formData.append('org_file_id', orgFileId);
+        formData.append('user_id', localStorage.getItem("user_id"))
+        formData.append('filled_form', file, file.name);
+
+        return axiosAPI.post('clearances/upload-user-file', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+            }
+        })
+        .then(({ thumbnail }) => thumbnail);
+    }
     
     const columns = [
         {
@@ -40,20 +42,42 @@ function UserFilesTable({orgId, fileList, messageHandler}) {
         {
           title: 'Upload Completed File',
           key: 'upload',
-          render: () => (
-            <Upload {...props} >
-                <Button icon={<UploadOutlined/>}>Upload</Button>  
+          render: (text, record, index) => (
+            <Upload {...props} 
+                beforeUpload={file => {
+                    console.log("key:", record.key)
+                    console.log("file:", record.file)
+                    console.log("index:", index)
+
+                    const formData = new FormData();
+                    formData.append('org_file_id', record.key);
+                    formData.append('user_id', localStorage.getItem("user_id"))
+                    formData.append('filled_form', file, file.name);
+
+                    return axiosAPI.post('clearances/upload-user-file', formData, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+                        }
+                    })
+                    .then(({ thumbnail }) => thumbnail);
+                }}
+            >
+                <Button icon={<UploadOutlined/> } onClick = {
+                    (e) => {
+                        
+                    }}
+                > Upload </Button>  
             </Upload>
           ),
         },
     ];
     console.log(fileList)
     
-    const data = fileList.map((file,i) => ({"key": i, "file": file.name}))
-    
+    const data = fileList.map((file,i) => ({"key": file.uid, "file": file.name}))
+
     return (
         <div>
-           <Table columns={columns} dataSource={data} />
+           <Table columns={columns} dataSource={data}/>
         </div>
     );
 };
