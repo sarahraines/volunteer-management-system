2e42f55b4f058c3cb797d601b7dcf9c1aaaddbe7
Sarah Raines <saraines@seas.upenn.edu>
2021-02-14 01:00:23 -0500

    add static root

--- server/server/settings.py
+++ server/server/settings.py
@@ -177,3 +177,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '..', 'static')
