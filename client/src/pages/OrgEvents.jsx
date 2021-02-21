448a7ca8debf361e25b0eb7b718555df4d51d387
LeeJ1721 <leej1721@wharton.upenn.edu>
2021-02-21 03:05:42 -0500

    added a filter for virtual or in person events

--- client/src/pages/OrgEvents.jsx
+++ client/src/pages/OrgEvents.jsx
@@ -1,13 +1,16 @@
 import React, {useState, useEffect, useCallback} from 'react';
-import {Button, Typography, message, Input} from 'antd';
+import {Button, Typography, message, Select, Space} from 'antd';
 import "antd/dist/antd.css";
 import './Event.css';
 import EventCard from '../components/EventCard';
 import axiosAPI from "../api/axiosApi";
 
+const { Option } = Select;
+
 const OrgEvents = ({orgId}) => {
     const [events, setEvents] = useState([]); 
     const [filterDisplay, setFilterDisplay] = useState([]);
+    const [attendeeCount, setAttendeeCount] = useState([]); 
 
     const getEventsByOrg = useCallback(async () => {
         try {
@@ -44,16 +47,89 @@ const OrgEvents = ({orgId}) => {
         }
     };
 
+    const virtualFilterChange = value => {
+        let oldList = events;
+        if (value == "virtual") {
+            let newList = [];
+            newList = oldList.filter(event =>
+                event.virtual
+            );
+            setFilterDisplay(newList);
+        } else if (value == "inperson") {
+            let newList = [];
+            newList = oldList.filter(event =>
+                !event.virtual
+            );
+            setFilterDisplay(newList);
+        } else {
+            setFilterDisplay(oldList);
+        }
+    };
+
+    const getAttendeeCount = useCallback(async (eventId) => {
+		try {
+			const response = await axiosAPI.get("events/get-event-attendee-count/", {
+                params: {
+					event: eventId
+                }
+            });
+            console.log("gets here");
+            setAttendeeCount(response.data);
+		}
+		catch {
+			const errMsg = "Get Attendee Count failed";
+			message.error(errMsg);
+		}
+    }, []);
+
+    // const openFilterChange = useCallback(async (value) => {
+    //     let oldList = events;
+    //     if (value == "open") {
+    //         let newList = [];
+    //         console.log("open")
+    //         newList = oldList.filter(e =>
+    //             {
+    //                 getAttendeeCount(e);
+    //                 console.log("attendee count" + attendeeCount);
+    //                 return (attendeeCount < e.attendee_cap)
+    //             }
+    //             // {console.log("attendee count " + getAttendeeCount(e.id));}
+    //         );
+    //         setFilterDisplay(newList);
+    //     } else if (value == "filled") {
+    //         let newList = [];
+    //         newList = oldList.filter(event =>
+    //             !event.virtual
+    //         );
+    //         setFilterDisplay(newList);
+    //     } else {
+    //         setFilterDisplay(oldList);
+    //     }
+    // }, []);
+
 
     return (
         <React.Fragment>
             <Typography.Title level={4}>Upcoming events</Typography.Title>
-            <input onChange={e => handleChange(e.target.value)} placeholder="Search for events"/>
+            <Space>
+                <input onChange={e => handleChange(e.target.value)} placeholder="Search for events" class="search"/>
+                Filter by Virtual:<Select defaultValue="none" style={{ width: 120 }} onChange={value => virtualFilterChange(value)} size="small">
+                    <Option value="none">Select All</Option>
+                    <Option value="virtual">Virtual</Option>
+                    <Option value="inperson">Not Virtual</Option>
+                </Select>
+                {/* Filter by Open:<Select defaultValue="none" style={{ width: 120 }} onChange={value => openFilterChange(value)} size="small">
+                    <Option value="none">Select All</Option>
+                    <Option value="open">Open</Option>
+                    <Option value="filled">Filled</Option>
+                </Select> */}
+            </Space> 
             <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between", overflowY: "scroll" }}>
                 {filterDisplay.map((item, i) => 
                     <EventCard key={i} item={item}/>
                 )}
             </div>
+            
         </React.Fragment>
     );
 }; export default OrgEvents;
