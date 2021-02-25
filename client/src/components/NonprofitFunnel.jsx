d74b58b505182ad5a4583d2778c326d0462cc164
Aruna Prasad <arunaprasad@Arunas-MacBook-Pro.local>
2021-02-25 10:20:59 -0500

    client/src/components/NonprofitFunnel.jsx

--- client/src/components/NonprofitFunnel.jsx
+++ client/src/components/NonprofitFunnel.jsx
@@ -6,11 +6,11 @@ function NonprofitFunnel({ data}) {
 
     return (
         <React.Fragment>
-            <Typography.Title level={5}>Goals</Typography.Title>
+            <Typography.Title level={5}>Summary</Typography.Title>
                 <Plotly data={[{
                     type: 'funnel', 
                     y: ["Number of Members", "Members who Attended Events", "Members who Provided Feedback"], 
-                    x: data}]} layout={{margin: {l: 250}, width:800, height: 500}}/>
+                    x: data}]} layout={{margin: {l: 250}, width:800}}/>
         </React.Fragment>
     )
 
