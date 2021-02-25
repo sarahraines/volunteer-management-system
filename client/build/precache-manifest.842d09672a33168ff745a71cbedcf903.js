da510830c539ed685a026385bc05437e3907f62c
Sarah Raines <saraines@seas.upenn.edu>
2021-02-24 22:22:35 -0500

    new build

---
+++ client/build/precache-manifest.842d09672a33168ff745a71cbedcf903.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e21f6f7000e4c4dfc734b5b0a010ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c3cc541c604686c1323",
+    "url": "/static/css/2.4754825d.chunk.css"
+  },
+  {
+    "revision": "d809dc58d119e801941e",
+    "url": "/static/css/main.95283c9b.chunk.css"
+  },
+  {
+    "revision": "0c3cc541c604686c1323",
+    "url": "/static/js/2.8b3a8539.chunk.js"
+  },
+  {
+    "revision": "0e9ef62f3bb315646d24c5928529123b",
+    "url": "/static/js/2.8b3a8539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d809dc58d119e801941e",
+    "url": "/static/js/main.557feed3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "50e5cddec2084ef052c41379f8b26d33",
+    "url": "/static/media/landing.50e5cdde.svg"
+  },
+  {
+    "revision": "303e4a676b033a04f22e2725f3624a66",
+    "url": "/static/media/login.303e4a67.svg"
+  },
+  {
+    "revision": "2c0812b63b5f46e0b2b489d216368799",
+    "url": "/static/media/register.2c0812b6.svg"
+  },
+  {
+    "revision": "98bf0bb301125c6a62afa89012ce60cc",
+    "url": "/static/media/volunteersense.98bf0bb3.png"
+  }
+]);
