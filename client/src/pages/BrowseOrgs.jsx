d4185b759c6315504eb742f7feb0a3523b656264
vbarth <vbarth@Vickys-MacBook-Pro-2.local>
2021-03-21 15:35:36 -0400

    added member from app

--- client/src/pages/BrowseOrgs.jsx
+++ client/src/pages/BrowseOrgs.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useCallback, useEffect} from 'react';
-import { Card, Typography } from 'antd';
+import { Card, Typography, Button, message } from 'antd';
 import axiosAPI from '../api/axiosApi';
 import './NewOrg.css';
 const { Title } = Typography;
@@ -20,12 +20,24 @@ function BrowseOrgs({}) {
         getPublicOrgs();
      }, [getPublicOrgs])
     
-   
+    const addMember = useCallback(async (name) => {
+        try {
+            await axiosAPI.post("member/create/", {
+                user_id: localStorage.getItem("user_id"),
+                organization: name,
+                member_type: 1,
+                status: 0,
+            });
+            message.success('Added to Organization');
+        } catch {
+            message.error('Unable to add to Organization');
+        }
+    }, []);
 
     return (
         orgs.length > 0 && (
             orgs.map(org => 
-            <Card title={org.name} extra={<a href="#">Join {org.name}</a>} style={{ width: 300 }}>
+            <Card title={org.name} extra={<Button type="primary" onClick= {() => addMember(org.name)} >Join {org.name}</Button>} style={{ width: 300 }}>
                     <p>{org.description}</p>
                     <p>{org.website}</p>
                     <p>{org.address}</p>
